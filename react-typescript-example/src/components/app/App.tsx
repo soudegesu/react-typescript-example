32d21ea56b741b0758849c2fed6b993a33b5c147
soudegesu <soudegesu@hotmail.co.jp>
2019-09-10 16:38:26 +0900

    import modules with absolute path

--- react-typescript-example/src/components/app/App.tsx
+++ react-typescript-example/src/components/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from '../../assets/images/logo.svg';
-import './App.css';
+import logo from 'assets/images/logo.svg';
+import 'components/app/App.css';
 
 const App: React.FC = () => {
   return (
